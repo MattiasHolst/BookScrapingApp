7ec6590dab569a341041f0d3818a218fd8e5e4fe
MattiasHolst <mattias@compare-it.se>
2024-04-09 23:45:52 +0200

    Able to navigate everywhere and see all "small images" except the ones on the startpage Next I need to be able to grab all "detail page images" aswell

--- BookScrapingApp/Program.cs
+++ BookScrapingApp/Program.cs
@@ -19,7 +19,7 @@ namespace BookScrapingApp
             web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
 
 			var client = new WebClient();
-			client.DownloadFile("https://books.toscrape.com/", "index.html");
+			client.DownloadFile(startPage, "index.html");
 
 			var startPageDocument = web.Load(startPage); 
 
@@ -32,29 +32,50 @@ namespace BookScrapingApp
 
 			}
 
-			//Images
-			var imageElements = startPageDocument.DocumentNode.QuerySelectorAll("img"); 
-			foreach(var imageElement in imageElements){
-				var cssUrl = HtmlEntity.DeEntitize(imageElement.Attributes["src"].Value); 
-				if(!Directory.Exists( Path.GetDirectoryName(cssUrl))) Directory.CreateDirectory(Path.GetDirectoryName(cssUrl));
-				client.DownloadFile(startPage+cssUrl, cssUrl);
 
-			}
-
-
-			/* Create folder structure to sidenav */
+			// Create folder structure with index files 
 			var sideNavElements = startPageDocument.DocumentNode.QuerySelectorAll("a");
 			foreach(var sideNavElement in sideNavElements){
 				var sideNavUrl = HtmlEntity.DeEntitize(sideNavElement.QuerySelector("a").Attributes["href"].Value);
-				Console.WriteLine("SideNavUrl is : " + sideNavUrl);
 				if(Path.GetDirectoryName(sideNavUrl) != ""){
 					if(!Directory.Exists( Path.GetDirectoryName(sideNavUrl))) Directory.CreateDirectory(Path.GetDirectoryName(sideNavUrl));
 				}
 				client.DownloadFile(startPage+sideNavUrl, sideNavUrl);
 
+				// Check if category folders got a Next button
+				if(Path.GetDirectoryName(sideNavUrl).Contains("category")){
+					Console.WriteLine("category : " + startPage+sideNavUrl);
+					var categoryPageDocument = web.Load(startPage+sideNavUrl); 
+					var nextButton = categoryPageDocument.DocumentNode.QuerySelector(".next > a");
+					var path = Path.GetDirectoryName(sideNavUrl);
+					while(nextButton != null){
+						var nextPageLink = nextButton.Attributes["href"].Value;
+						client.DownloadFile(startPage+path+"/"+nextPageLink, path+"/"+nextPageLink);
+						var nextButtonPageDocument = web.Load(startPage+path+"/"+nextPageLink);
+						nextButton = nextButtonPageDocument.DocumentNode.QuerySelector(".next > a");
+						// Small Images
+						var imageElements = nextButtonPageDocument.DocumentNode.QuerySelectorAll("img"); 
+						foreach(var imageElement in imageElements){
+							var imageUrl = HtmlEntity.DeEntitize(imageElement.Attributes["src"].Value); 
+							if(!Directory.Exists( Path.GetDirectoryName(imageUrl))) Directory.CreateDirectory(Path.GetDirectoryName(path+"/"+imageUrl));
+							if(!File.Exists(imageUrl)) client.DownloadFile(startPage+imageUrl, path+"/"+imageUrl);
+
+						}
+						// Articles
+						var articleElements = nextButtonPageDocument.DocumentNode.QuerySelectorAll(".product_pod"); 
+						foreach(var articleElement in articleElements){
+							var detailPageUrl = HtmlEntity.DeEntitize(articleElement.QuerySelector("a").Attributes["href"].Value); 
+							if(!Directory.Exists( Path.GetDirectoryName(detailPageUrl))) Directory.CreateDirectory(Path.GetDirectoryName(path+"/"+detailPageUrl));
+							if(!File.Exists(detailPageUrl)) client.DownloadFile(startPage+path+"/"+detailPageUrl, path+"/"+detailPageUrl);
+						}
+					}
+
+					
+				}
 
 			}
            
-		} 
+		}
+	
 	} 
 }
