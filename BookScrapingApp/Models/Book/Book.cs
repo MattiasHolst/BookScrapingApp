439941c930ce7a1f4216cfa7891d334df8dff256
MattiasHolst <mattias@compare-it.se>
2024-04-11 11:21:35 +0200

    Removed old file

--- BookScrapingApp/Models/Book/Book.cs
+++
@@ -1,9 +0,0 @@
-namespace BookScrapingApp{
-    public class Book 
-		{ 
-			public string? Url { get; set; } 
-			public string? Image { get; set; } 
-			public string? Name { get; set; } 
-			public string? Price { get; set; } 
-		} 
-}
