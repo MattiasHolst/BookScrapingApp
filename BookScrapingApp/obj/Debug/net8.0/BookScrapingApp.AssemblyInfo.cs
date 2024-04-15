330a8d46a25aa5e868fbd808d1a764243218b32c
MattiasHolst <mattias@compare-it.se>
2024-04-15 14:15:44 +0200

    Fully commited all files to work on a clean slate

--- BookScrapingApp/obj/Debug/net8.0/BookScrapingApp.AssemblyInfo.cs
+++ BookScrapingApp/obj/Debug/net8.0/BookScrapingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookScrapingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e68eb0fd335c88fe98d9abefca0c2dd860880d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+439941c930ce7a1f4216cfa7891d334df8dff256")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookScrapingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookScrapingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
